1b76f26e4b87d95a1b9512e48541f1f0a8aaab9e
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-05-21 20:00:02 +0200

    Publish

---
+++ docs/precache-manifest.6c7873d0ffb41d15954496272aef3995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f68c3f52888dcbc6ccbf0f45e5024f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "163b843a73fb7995aef9",
+    "url": "./static/css/main.aec539db.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "./static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "163b843a73fb7995aef9",
+    "url": "./static/js/main.85d3c67f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
