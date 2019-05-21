fab2d3854ced5d39d1f999907751e21f98c4d2e2
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-05-21 17:47:55 +0200

    publish

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 import './scss/main.scss';
 import PokeList from './Components/PokeList';
 
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class Pokemon extends React.Component{
   render(){
@@ -8,13 +10,16 @@ class Pokemon extends React.Component{
         <img className="pokemon__img" src={item.url} alt={item.name}/>
         <h2 className="pokemon__name">{item.name}</h2>
         <ul className="pokemon__type">
-        {item.types.map((obj, index)=>      <li className="pokemon__pwr" key={index}>{obj}</li>
+        {item.types.map((obj, index)=>      
+        <li className="pokemon__pwr" key={index}>{obj}</li>
         )}
         </ul>
       </div>    
     );
   }
 }
-
+Pokemon.propTypes = {
+  styling: PropTypes.string,
+}
 export default Pokemon;
 
